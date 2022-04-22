9c193b8e0df78bbf6aea8e9a77d014da96eb34f1
wasand <wasand@o2.pl>
2022-04-22 10:18:15 +0200

    RED

--- testCalc.py
+++ testCalc.py
@@ -4,11 +4,13 @@ from calc import Calc
 num1 = 1
 num2 = 2
 
+
 def test_add():
     assert Calc.add(num1, num2) == 3
 
-def test_int_max( num1, num2):
-    assert Calc.int_max( num1, num2) == 2
+
+def test_int_max():
+    assert Calc.int_max(num1, num2) == 2
 
 
 
